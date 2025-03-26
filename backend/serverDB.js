8830ef77c4786c4d42c6d08ce3dcc88698ec5329
Juan Jaramillo <jcarlosjaramillo@yahoo.com>
2025-03-26 14:17:17 +0000

    cambio de nombre de db

--- backend/serverDB.js
+++ backend/serverDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const username = 'user_mongo'
 const password = 'dobleq3'
-const dbname = 'electronica'
+const dbname = 'dev'
 const string_connection = `mongodb://mongo_user:dobleq3@mongo_cont:27017/dev?authSource=admin`
 
 try{
