c10e4df736eeb45c88fa1ff4e3833570e61c2951
Juan Jaramillo <jcarlosjaramillo@yahoo.com>
2025-03-26 14:13:14 +0000

    correcion en string connection mongodb

--- backend/serverDB.js
+++ backend/serverDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const username = 'user_mongo'
 const password = 'dobleq3'
 const dbname = 'electronica'
-const string_connection = `mongodb://mongo_user:dobleq3@mongo_cont:27017/data?authSource=admin`
+const string_connection = `mongodb://mongo_user:dobleq3@mongo_cont:27017/dev?authSource=admin`
 
 try{
   mongoose.connect(string_connection) 
